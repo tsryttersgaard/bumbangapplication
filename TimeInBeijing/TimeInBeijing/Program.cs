7c5df2917b191e247a1f71cb88a0e779aebb4462
Thomas Senn Ryttersgaard <thomas@ryttersgaard.dk>
2018-08-18 12:24:09 +0200

    The Beijing challenge

---
+++ TimeInBeijing/TimeInBeijing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace TimeInBeijing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Date/time GMT: {0}", DateTime.UtcNow);
+            Console.WriteLine("Date/time Copenhagen: {0}", GetTimeZoneTimestamp("København"));
+            Console.WriteLine("Date/time Beijing: {0}", GetTimeZoneTimestamp("Beijing"));
+            Console.ReadLine();
+        }
+
+        static string GetTimeZoneTimestamp(string cityName)
+        {
+            var zone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.DisplayName.Contains(cityName));
+            return zone != null ? TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, zone).ToString() : "(not found)";
+        }
+    }
+}
